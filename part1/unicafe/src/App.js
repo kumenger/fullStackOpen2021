387f669550ca0abbde95efea415c9e90acdf7e24
kumenger <44558777+kumenger@users.noreply.github.com>
2021-06-20 00:47:04 -0600

    table form

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,48 +1,42 @@
 import React ,{useState} from 'react'
  const Statistics=(props)=>{
    if(props.good===0&&props.neutral===0&&props.Bad===0){
-     return <div style={{color:"red",textAlign:"center"}}>No feedBack given!!</div>
+     return <div style={{color:"red",textAlign:"left"}}><br></br>No feedBack given!!</div>
    }
    return <div>
-     <h2 style={{textAlign:"center"}}>Statistics</h2>
-     <div style={{textAlign:"center"}}>
-       <table class='center' style={{textAlign:"center"}} >
-         <tr>
-           <td><h3>Good</h3></td>
-           <td><strong style={{color:"red",fontSize:"22px"}}>{props.good}</strong></td>
-         </tr>
-         <tr>
-           <td><h3>Neutral</h3></td>
-           <td><strong style={{color:"red",fontSize:"22px"}}>{props.neutral}</strong></td>
-         </tr>
-         <tr>
-           <td><h3>Bad</h3></td>
-           <td><strong style={{color:"red",fontSize:"22px"}}>{props.Bad}</strong></td>
-         </tr>
-         <tr>
-           <td><h3>Total</h3></td>
-           <td><strong style={{color:"red",fontSize:"22px"}}>{props.total}</strong></td>
-         </tr>
-         <tr>
-           <td><h3>Average</h3></td>
-           <td><strong style={{color:"red",fontSize:"22px"}}>{props.average}</strong></td>
-         </tr>
-         <tr>
-           <td><h3>Postive</h3></td>
-           <td><strong style={{color:"red",fontSize:"22px"}}>{props.postive}</strong> </td>
-         </tr>
+     <h2 style={{textAlign:"left"}}>Statistics</h2>
+     <div style={{textAlign:"left"}}>
+       <table  style={{textAlign:"left"}} >
+         <tbody><Statistic text="Good" value={props.good} />
+         <Statistic text="Neutral" value={props.neutral} />
+         <Statistic text="Bad" value={props.Bad} />
+         <Statistic text="Total" value={props.total} />
+         <Statistic text="Average" value={props.average} />
+         <Statistic text="Postive" value={props.postive+"%" } /></tbody>
+         
+        
        </table>
              </div>
 
    </div>
  }
+ const Statistic=(props)=>{
+   return <>
+   <tr>
+           <td><h3>{props.text}</h3></td>
+           <td><strong style={{color:"red",fontSize:"22px"}}>{props.value}</strong></td>
+         </tr>
+   </>
+
+   
+ }
  const Button=(props)=>{
-   return <><button onClick={props.handleClick} style={{height:"40px",textAlign:"center"}}>{props.text}</button></>
+   return <><button onClick={props.handleClick} style={{height:"40px",textAlign:"left"}}>{props.text}</button></>
  }
 const App=()=>{
   
   const[feedBack,SetFeedBack]=useState({good:0,Neutral:0,Bad:0})
-  console.log(feedBack.good,feedBack.Neutral,feedBack.Bad)
+  
   const handleClickGood=()=>{
    const newFeedBack={...feedBack,good:feedBack.good+1}
    SetFeedBack(newFeedBack)
@@ -56,16 +50,16 @@ const App=()=>{
     SetFeedBack(newFeedBack)
    } 
  let sums=feedBack.good+feedBack.Neutral+feedBack.Bad
-  return(<div style={{textAlign:"center"}} >
-    <h1 style={{textAlign:"center"}}>Give FeedBack</h1>
-    <div style={{padding:"10%,0,10%,0",textAlign:"center"}}>
+  return(<div style={{textAlign:"left"}} >
+    <h1 style={{textAlign:"left"}}>Give FeedBack</h1>
+    <div style={{padding:"10%,0,10%,0",textAlign:"left"}}>
       
      <Button text="Good" handleClick={handleClickGood}/>&nbsp;
      <Button text="Neutral" handleClick={handleClickNeutral}/>&nbsp;
      <Button text="Bad" handleClick={handleClickBad}/>&nbsp;
     </div>
    
-    <Statistics good={feedBack.good } neutral={feedBack.Neutral} Bad={feedBack.Bad}  total={sums} average={sums/3} postive={feedBack.good/sums}/>
+    <Statistics good={feedBack.good } neutral={feedBack.Neutral} Bad={feedBack.Bad}  total={sums} average={sums/3} postive={feedBack.good/sums*100}/>
     
      
 
